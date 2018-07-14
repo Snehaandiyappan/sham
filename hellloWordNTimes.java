1e24d6c6e368a4be21be7155d02ad8fa6364f1fb
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 15:42:41 +0530

    Create hellloWordNTimes.java

---
+++ hellloWordNTimes.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class hello
+{
+	public static void main (String[] args)
+	{
+	Scanner in=new Scanner(System.in);
+	System.out.println("how many time do you want to print the String:");
+	int a=in.nextInt();
+	String s=in.next();
+	int count=0;
+for(int i=1;i<=a;i++){
+			System.out.println(s);
+		}
+	}
+}
