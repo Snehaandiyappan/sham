2b42b294f9ad9b6e40cada74f2f79d3d716aabc6
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 14:58:44 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class reverse {
+public static void main(String[] args){
+	Scanner in=new Scanner(System.in);
+	String s=in.nextLine();
+	StringBuffer s1=new StringBuffer(s);
+	System.out.println(s1.reverse());
+	in.close();
+}
+}
