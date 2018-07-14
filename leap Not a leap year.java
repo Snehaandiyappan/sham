6c81ead1993f92c8c2c100a3324fea0afb7c4695
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 16:03:21 +0530

    Create leap Not a leap year.java

---
+++ leap Not a leap year.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args)
+	{
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+if(a%4==0){
+	
+	System.out.println("yes");
+		}
+else{
+	System.out.println("No");
+
+}
+	}
+}
