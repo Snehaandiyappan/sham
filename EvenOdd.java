eabc51f2f156043c86f7ba7c82b9ba6b8f62c74b
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 14:05:29 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		if(a%2==0){
+			System.out.print("EVEN");
+		}else{
+			System.out.print("ODD");
+		}
+	}
+}
