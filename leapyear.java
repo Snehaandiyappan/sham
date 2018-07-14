c676a5e6477aa68224a63460c33d2158bd1f67c8
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 16:08:37 +0530

    Update and rename leap Not a leap year.java to leapyear.java

--- leapyear.java
+++ leapyear.java
@@ -5,13 +5,11 @@ class Ideone
 	{
 	Scanner in=new Scanner(System.in);
 	int a=in.nextInt();
-if(a%4==0){
-	
-	System.out.println("yes");
-		}
+ if(a%4==0){
+	 System.out.println("yes");
+	}
 else{
 	System.out.println("No");
-
 }
-	}
+}
 }
