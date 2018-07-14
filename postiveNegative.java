9b899bbefc3d41dc4c64800e2ab89a977197558f
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-14 13:58:31 +0530

    Update and rename postiveNegative to postiveNegative.java

--- postiveNegative.java
+++ postiveNegative.java
@@ -2,8 +2,11 @@ import java.util.*;
 public static void main(String[] args){
 Scanner in=new Scanner(System.in);
 int a=in.nextInt();
-if(a>=0){
+if(a>0){
 System.out.print("POSITIVE");
-}
+}else if(a<0){
 System.out.print("NEGATIVE");
+}
+System.out.print("ZERO");
+
 }
