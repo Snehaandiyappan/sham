a167b1e8992d12b8a5b8e43c96fe53adadf080d7
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-15 11:56:17 +0530

    Create Naturalno.java

---
+++ Naturalno.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+{
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int sum=0;
+	for(int i=1;i<=a;i++){
+		sum+=i;
+	}
+	System.out.println(sum);
+}
+}
