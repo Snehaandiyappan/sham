7d2e621c64112eea87b488655cee2e4590e93846
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-15 12:14:07 +0530

    Create powerOfNo..java

---
+++ powerOfNo..java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+{
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int power=in.nextInt();
+	System.out.println(Math.pow(n,power));
+}
+}
