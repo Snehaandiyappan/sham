9b49dc5a966daace000b7f1ba17964a9870b1828
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-15 10:30:02 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Vowel
+{
+	public static void main (String[] args) 
+{
+	Scanner in=new Scanner(System.in);
+	char a=in.next().charAt(0);
+if(a=='a' ||a=='A' ||a=='e' ||a=='E' ||a=='i' ||a=='I'||a=='o' ||a=='O' ||a=='u' ||a=='U'){
+	System.out.println("vowel");
+}
+else if(a>='b' && a<='z' || a>='B' && a<='z'){
+		System.out.println("Constant");
+}else{
+		System.out.println("Invalid");
+}
+	}
+}
