a2b1d83e09c15119b3941c3cb8e2ea042cee69a0
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-16 09:54:13 +0530

    Create nEven.java

---
+++ nEven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Ideone
+{
+   public static void main (String[] args)
+   {		
+      Scanner in = new Scanner(System.in);
+     int a=in.nextInt();
+     int b=in.nextInt();
+     for(int i=a+1;i<b;i++){
+    		 if(i%2==0){
+    			 System.out.print(i +" ");
+    			 }
+    			}
+     in.close();
+    	 }
+    }
