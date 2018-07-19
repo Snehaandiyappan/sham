c1ebcf85a5791ef5081e4d292cfba74bce61c37e
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-19 14:55:08 +0530

    Create sort.java

---
+++ sort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Ideone
+{
+   public static void main (String[] args)
+   {		
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+     int a[]=new int[n];
+     int temp=0;
+    for(int i=0;i<n;i++){
+    	a[i]=in.nextInt();
+    }
+    for(int i=0;i<n;i++){
+    	for(int j=i+1;j<n;j++){
+    	if(a[i]>a[j]){
+    		temp=a[i];
+    		a[i]=a[j];
+    		a[j]=temp;
+    	}
+    }
+    }for(int i=0;i<n;i++){
+    System.out.println(a[i]);
+    }
+     in.close();
+     }
+    }
