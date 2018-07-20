02b72df7c3428e9e7e39ed6d38410baf14f59b77
ShiyamaladeviS <41099594+ShiyamaladeviS@users.noreply.github.com>
2018-07-20 09:43:00 +0530

    Create appendp.java

---
+++ appendp.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Ideone
+{
+   public static void main (String[] args)
+   {		
+      Scanner in = new Scanner(System.in);
+     String str=in.nextLine();
+     System.out.println(str+".");
+     in.close();
+     }
+    }
